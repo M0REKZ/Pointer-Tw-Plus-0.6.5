5cb4beb18622ee91d9830eca9763ce57af847ad8
+KZ <elmaestrodelsurvivalcraft@gmail.com>
2024-12-03 12:05:26 -0300

    fix merge bug

--- src/game/collision.h
+++ src/game/collision.h
@@ -11,6 +11,15 @@ class CCollision
 	int m_Width;
 	int m_Height;
 	class CLayers *m_pLayers;
+	
+	struct telePos
+	{
+		int x;
+		int y;
+		bool exists = false;
+	};
+	
+	telePos m_telePositions[4]; // positions of teleports
 
 	bool IsTileSolid(int x, int y);
 	int GetTile(int x, int y);
@@ -22,11 +31,16 @@ public:
 		COLFLAG_SOLID=1,
 		COLFLAG_DEATH=2,
 		COLFLAG_NOHOOK=4,
+		COLFLAG_TELEONE=8,
+		COLFLAG_TELETWO=16,
+		COLFLAG_TELETHREE=32,
+		COLFLAG_TELEFOUR=64,
+		COLFLAG_SLOWDEATH=128,
 	};
 
 	CCollision();
 	void Init(class CLayers *pLayers);
-	bool CheckPoint(float x, float y) { return IsTileSolid(round(x), round(y)); }
+	bool CheckPoint(float x, float y) { return IsTileSolid(roundbyteeworlds(x), roundbyteeworlds(y)); }
 	bool CheckPoint(vec2 Pos) { return CheckPoint(Pos.x, Pos.y); }
 	int GetCollisionAt(float x, float y) { return GetTile(roundbyteeworlds(x), roundbyteeworlds(y)); }
 	int GetCollisionAtNew(float x, float y) { return GetTileNew(roundbyteeworlds(x), roundbyteeworlds(y)); }
@@ -36,6 +50,8 @@ public:
 	void MovePoint(vec2 *pInoutPos, vec2 *pInoutVel, float Elasticity, int *pBounces);
 	void MoveBox(vec2 *pInoutPos, vec2 *pInoutVel, vec2 Size, float Elasticity);
 	bool TestBox(vec2 Pos, vec2 Size);
+	int getTeleX(int index);
+	int getTeleY(int index);
 };
 
 #endif
